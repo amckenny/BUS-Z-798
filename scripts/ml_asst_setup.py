380bfe2f10a894bb34dca74347420c8c92917863
McKenny, Aaron <amckenny@iu.edu>
2024-03-21 16:50:49 -0400

    changes nltk posix path to str

--- scripts/ml_asst_setup.py
+++ scripts/ml_asst_setup.py
@@ -8,7 +8,7 @@ def main():
     print("Getting NLTK Models")
     nltk_dir = Path.cwd()/"models"
     assert nltk_dir.exists()
-    os.environ["NLTK_DATA"] = nltk_dir
+    os.environ["NLTK_DATA"] = str(nltk_dir)
     nltk.download('stopwords', quiet=True)
     nltk.download('vader_lexicon', quiet=True)
 
